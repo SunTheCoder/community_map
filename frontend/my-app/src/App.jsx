c9f5b90bf2e28618f98344a1358e2e8b3e082236
SunTheCoder <sunenglishjr@gmail.com>
2024-11-07 11:43:58 -0500

    add frontend

---
+++ frontend/my-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ResourceList from './components/Resources/resourceList'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    
+    <ResourceList/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/my-app/src/components/Resources/resourceList.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import api from '../../api';  // Import the custom Axios instance
+import './ResourceList.css';  // Import the CSS file for styling
+
+const ResourceList = () => {
+    const [resources, setResources] = useState([]);
+
+    useEffect(() => {
+        // Fetch resources from the API when the component mounts
+        api.get('/resources')
+           .then(response => setResources(response.data))
+           .catch(error => console.error(error));
+    }, []);
+
+    console.log('Resources:', resources);
+    return (
+        <div>
+            <h2>Resource List</h2>
+            {resources.map(resource => (
+                console.log('Resource:', resource),  // Log the resource for debugging purposes
+                <div key={resource.id}>
+                    <h3>{resource.name}</h3>
+                    <h4>{resource.type}</h4>
+                    <p>{resource.comments}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default ResourceList;
